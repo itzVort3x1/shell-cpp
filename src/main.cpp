66bb5480de5dcc5cc9ad88173c31eb8dd2a539ec
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-08 10:43:51 +0000

    updated code for type shell builtin

--- src/main.cpp
+++ src/main.cpp
@@ -3,53 +3,63 @@
 using namespace std;
 
 int main() {
-  // Flush after every std::cout / std:cerr
+  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
+  // Define the set of shell builtins
+  unordered_set<string> shellBuiltins = {"exit", "echo", "type"};
 
-  while(true){
-
+  while (true) {
     cout << "$ ";
     string input;
     getline(cin, input);
 
-    // handling empty input
-    if(input.empty()){
+    // Handling empty input
+    if (input.empty()) {
       continue;
-    } 
+    }
 
     size_t spacePos = input.find(' ');
     string command;
     string arguments;
 
-    if(spacePos != string::npos){
+    if (spacePos != string::npos) {
       command = input.substr(0, spacePos);
-      arguments = input.substr(spacePos+1);
-    }else {
+      arguments = input.substr(spacePos + 1);
+    } else {
       command = input;
     }
 
-    if(command == "exit"){
+    if (command == "exit") {
       int exitCode = 0;
-      if(!arguments.empty()){
+      if (!arguments.empty()) {
         try {
           exitCode = stoi(arguments);
-        }catch (std::invalid_argument &e){
+        } catch (std::invalid_argument &e) {
           cerr << "Invalid exit code. Exiting with default code 0." << endl;
-        }catch (std::out_of_range &e){
+        } catch (std::out_of_range &e) {
           cerr << "Exit code out of range. Exiting with default code 0." << endl;
         }
       }
       return exitCode;
-    }else if(command == "echo"){
+    } else if (command == "echo") {
       cout << arguments << endl;
-    }else {
-      cerr << command + ":" << " command not found" << endl;
+    } else if (command == "type") {
+      if (arguments.empty()) {
+        cerr << "type: usage: type [command]" << endl;
+        continue;
+      }
+
+      // Check if the argument is a shell builtin
+      if (shellBuiltins.find(arguments) != shellBuiltins.end()) {
+        cout << arguments << " is a shell builtin" << endl;
+      } else {
+        cerr << command << ": command not found" << endl;
+      }
+    } else {
+      cerr << command << ": command not found" << endl;
     }
-
-
   }
 
   cout << endl;
