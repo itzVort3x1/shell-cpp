3c3fd7029e451a91a50b622738af5e45bba61e42
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-09 12:18:21 +0000

    updated directory reading

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <unordered_set>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <string>
+#include <dirent.h>
+#include <vector>
 using namespace std;
 
 string BIN_PATH="/usr/bin/";
@@ -9,21 +12,49 @@ string LOCAL_PATH="/usr/local/bin/";
 unordered_set<string> SHELLBUILTINS = {"exit", "echo", "type", "abcd", "ls"};
 
 
-void searchBuiltIn(string &args){
-  string binFullPath = BIN_PATH + args;
-  string localFullPath = LOCAL_PATH + args;
-  const char *binSearchPath = binFullPath.c_str();
-  const char *localSearchPath = localFullPath.c_str();
-  struct stat sb;
+bool searchFileInDirectory(const string &directory, const string &filename, string &foundPath) {
+    struct stat sb;
+    string fullPath = directory + "/" + filename;
 
-  if(stat(binSearchPath, &sb) == 0){
-    cout << args << " is " << binFullPath << endl;
-  }else if(stat(localSearchPath, &sb) == 0){
-    cout << args << " is " << localSearchPath << endl;
-  }else {
-    cerr << args << ": not found" << endl;
-  }
+    if (stat(fullPath.c_str(), &sb) == 0 && S_ISREG(sb.st_mode)) {
+        foundPath = fullPath;
+        return true;
+    }
+    return false;
 }
+
+void searchBuiltIn(const string &filename) {
+    string foundPath;
+
+    // Get directories from PATH environment variable
+    const char *pathEnv = getenv("PATH");
+    vector<string> searchPaths;
+
+    if (pathEnv) {
+        string pathStr = pathEnv;
+        size_t pos = 0;
+        while ((pos = pathStr.find(':')) != string::npos) {
+            searchPaths.push_back(pathStr.substr(0, pos));
+            pathStr.erase(0, pos + 1);
+        }
+        searchPaths.push_back(pathStr); // Add the last segment
+    }
+
+    // Prioritize specific directories (e.g., /usr/bin)
+    searchPaths.insert(searchPaths.begin(), {"/usr/bin", "/bin"});
+
+    // Search in prioritized directories
+    for (const string &dir : searchPaths) {
+        if (searchFileInDirectory(dir, filename, foundPath)) {
+            cout << filename << " is " << foundPath << endl;
+            return;
+        }
+    }
+
+    // If not found in PATH, fallback to a full filesystem search
+    cerr << filename << ": not found" << endl;
+}
+
 
 int main() {
   // Flush after every std::cout / std::cerr
